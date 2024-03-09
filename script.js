3c00e98d1cfaf85fdcdb712d6357b4cb0c538646
Bridget Amana <amanabridget03@gmail.com>
2024-03-10 00:55:27 +0100

    Added movie search functionality

---
+++ script.js
@@ -0,0 +1,45 @@
+const movieContainer = document.getElementById('movie-container');
+const key = '1ec7b992';
+const searchBox = document.getElementById('search-box');
+const form = document.querySelector('form');
+
+// Function to fetch movies based on the search query
+const fetchMovies = (title) => {
+    fetch(`http://www.omdbapi.com/?s=${title}&apikey=${key}`)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.Response === "True") {
+                const movies = data.Search;
+                movies.forEach((movie) => {
+                    const movieCard = `
+                        <div class="movie-card">
+                            <div class="movie-img">                    
+                                <img src="${movie.Poster}" alt="${movie.Title}" />
+                            </div>
+                            <span class="title-wrapper">
+                                <h3 class="movie-title">${movie.Title}</h3>
+                                <p class='movie-year'>Year: ${movie.Year}</p>
+                            </span>
+                            <button>See Details</button>
+                        </div>
+                    `;
+                    movieContainer.innerHTML += movieCard;
+                });
+            } else {
+                movieContainer.innerHTML = '<p class="error-msg">No movies found with that title</p>';
+            }
+        })
+        .catch((err) => {
+            movieContainer.innerHTML = '<p class="error-msg">There was an error loading the movies</p>';
+        });
+};
+
+// Event listener for form submission
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = searchBox.value.trim();
+    if (searchTerm !== '') {
+        fetchMovies(searchTerm);
+    }
+});
+
